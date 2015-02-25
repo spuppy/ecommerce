db38cebbfa64735868fad86c77b541dabdd3f450
spuppy <jugdei@hotmail.com>
2015-02-25 19:21:43 +0700

    Remove from cart

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -1,18 +1,21 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from django.conf import settings
+
 from django.conf.urls.static import static
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ecommerce.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),	
     url(r'^$', 'products.views.home', name='home'),
-    url(r'^cart/$', 'carts.views.view', name='cart'),
+    url(r'^cart/(?P<id>\d+)/$', 'carts.views.remove_from_cart', name='remove_from_cart'),
     url(r'^cart/(?P<slug>[\w-]+)/$', 'carts.views.add_to_cart', name='add_to_cart'),
-    #see. temlate\cart\view.html
-     url(r'^s/$', 'products.views.search', name='search'),
+    
+    url(r'^cart/$', 'carts.views.view', name='cart'),
+    url(r'^s/$', 'products.views.search', name='search'),
     url(r'^products/$', 'products.views.all', name='products'),
     url(r'^products/(?P<slug>[\w-]+)/$', 'products.views.single', name='single_product'),
     #Note \d+ == only digit
